152b35c69c781a5da0c1ecdadf226030109b0e89
Carl <94771147+BeetJuicer@users.noreply.github.com>
2024-09-24 13:41:01 +0800

    ginoworld example

--- Assets/Scripts/BuildingOverlay.cs
+++ Assets/Scripts/BuildingOverlay.cs
@@ -99,7 +99,6 @@ public class BuildingOverlay : MonoBehaviour
     private void MoveIncrementallyRight()
     {
         transform.position += transform.right * incrementalMovementUnits;
-
     }
 
     [Button] 
