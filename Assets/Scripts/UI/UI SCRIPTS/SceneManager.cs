1927e46352af47f7770fc2748a0d760eb07d944e
Vaneric G. San Pascual <159170035+Muzukash1@users.noreply.github.com>
2024-09-26 13:59:49 +0800

    MainMenu Background
    Clickable Play hindi ko lang alam kung saan mag play settings clickable kaso wala pang ui na nagagawa para sa settings
    
    background temporary lang

--- Assets/Scripts/UI/UI SCRIPTS/SceneManager.cs
+++ Assets/Scripts/UI/UI SCRIPTS/SceneManager.cs
@@ -1,9 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
+    internal static object GetActiveScene()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void LoadScene(object value)
+    {
+        throw new NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
