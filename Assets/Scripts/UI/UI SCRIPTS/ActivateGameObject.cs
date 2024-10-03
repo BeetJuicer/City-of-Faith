7c1e9ff6f8db2c338583fb5b6279ac2536086501
Xiaoranxi <164878289+Xiaoranxi@users.noreply.github.com>
2024-10-03 15:34:32 +0800

    ShopInterface

---
+++ Assets/Scripts/UI/UI SCRIPTS/ActivateGameObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateGameObject : MonoBehaviour
+{
+    // Drag the GameObject you want to activate in the Inspector
+    public GameObject objectToActivate;
+
+    // This method will be called when the button is clicked
+    public void ActivateObject()
+    {
+        objectToActivate.SetActive(true); // Activate the GameObject
+    }
+
+    // Drag the GameObject you want to Deactivate in the Inspector
+    public GameObject objectToDeactivate;
+
+    // This method will be called when the button is clicked
+    public void DeactivateObject()
+    {
+        objectToDeactivate.SetActive(false); // Deactivate the GameObject
+    }
+
+}
+
