3eca6f33277451f99e6b92120a6033b8aed55a00
Carl <94771147+BeetJuicer@users.noreply.github.com>
2024-11-14 21:40:35 +0800

    Update Plot.cs

--- Assets/Scripts/Plot.cs
+++ Assets/Scripts/Plot.cs
@@ -20,13 +20,17 @@ public class Plot : MonoBehaviour, IClickableObject
 
 
     //TODO: Temporary serialize field for debugging
-    [SerializeField] private Crop_SO crop_SO;
+    [SerializeField] Crop_SO tempCropSO;
+
+    private CropVisual cropVisual;
+    private Crop_SO crop_SO;
     [SerializeField] private Transform cropVisualPos;
 
     // Database
     private Database db;
     private Database.PlotData plotData = null;
 
+    //debugging
     private PlotState currentPlotState = PlotState.WAITING;
     public PlotState CurrentPlotState
     {
@@ -37,10 +41,11 @@ public class Plot : MonoBehaviour, IClickableObject
             currentPlotState = value;
             plotData.plot_state = (int)currentPlotState;
             db.UpdateRecord(plotData);
+            if (cropVisual != null) cropVisual.UpdateVisual(currentPlotState);
         }
     }
 
-    public Crop_SO Crop_SO { get { return crop_SO; } set { } }
+    public Crop_SO Crop_SO { get { return crop_SO; } set { crop_SO = value; } }
     private DateTime growthFinishTime;
     public DateTime GrowthFinishTime
     {
@@ -62,12 +67,14 @@ public class Plot : MonoBehaviour, IClickableObject
 
         if (plotData == null)
         {
+            Crop_SO = tempCropSO;
             db = FindFirstObjectByType<Database>();
+
             plotData = new Database.PlotData
             {
                 structure_id = structure.StructureID,
                 growth_finish_time = GrowthFinishTime,
-                plot_state = (int)CurrentPlotState,
+                plot_state = (int)currentPlotState,
                 crop_so_name = Crop_SO.name
             };
 
@@ -77,21 +84,24 @@ public class Plot : MonoBehaviour, IClickableObject
 
     public void LoadData(Database.PlotData data, Database db)
     {
+        //not using the property so that I don't have to call the update database event. Just loading data. 
         plotData = data;
         this.db = db;
 
-        CurrentPlotState = (PlotState)data.plot_state;
-        //not using the property so that I don't have to call the update database event. Just loading data. 
+        currentPlotState = (PlotState)data.plot_state;
         growthFinishTime = data.growth_finish_time;
 
-        string path = $"Crop/{data.crop_so_name}";
+        string path = $"Crops/SO_Crops/{data.crop_so_name}";
         Crop_SO cropSO = (Crop_SO)Resources.Load(path);
         Debug.Assert(cropSO != null, $"{data.crop_so_name} does not exist in {path}!");
 
         crop_SO = cropSO;
 
-        if (CurrentPlotState == PlotState.GROWING)
-            Instantiate(cropSO.cropPrefab, cropVisualPos);
+        if (currentPlotState == PlotState.GROWING ||
+            currentPlotState == PlotState.RIPE)
+        {
+            InstantiateCrop(cropSO);
+        }
     }
 
     void Update()
@@ -145,29 +155,17 @@ public class Plot : MonoBehaviour, IClickableObject
 
     }
 
-    /// <summary>
-    ///  Method to be used when a plant is chosen in the shop menu.\
-    /// </summary>
-    /// <param name="crop_SO"></param>
-    public void Plant(Crop_SO crop_SO)
+    // Calls update visual on the crop as well.
+    private void InstantiateCrop(Crop_SO crop_SO)
     {
-        this.crop_SO = crop_SO;
-
-        // Asserts
-        Debug.Assert(crop_SO == null, "crop SO cannot be null!");
-        Debug.Assert(CurrentPlotState == PlotState.EMPTY, "Plot must be empty to plant!");
-        Debug.Assert(crop_SO.baseTimeNeededPerClaim >= TimeSpan.Zero, "Time needed cannot be negative!");
-
-        // Success
-        GrowthFinishTime = DateTime.Now.Add(crop_SO.baseTimeNeededPerClaim);
-        CurrentPlotState = PlotState.GROWING;
-
-        //Visual update
-        //TODO: Possible optimization, use crop pools.
-        var crop = Instantiate(crop_SO.cropPrefab, cropVisualPos);
-        if(crop.TryGetComponent(out CropVisual cropVisual))
+        var cropGO = Instantiate(crop_SO.cropPrefab, cropVisualPos);
+        if (cropGO.TryGetComponent(out cropVisual))
         {
             cropVisual.UpdateVisual(CurrentPlotState);
+        }
+        else
+        {
+            Debug.LogWarning($"{crop_SO.cropPrefab} does not have a CropVisual component.");
         }
     }
 
@@ -175,7 +173,29 @@ public class Plot : MonoBehaviour, IClickableObject
     [Button]
     public void Plant()
     {
-        Plant(crop_SO);
+        Plant(tempCropSO);
+    }
+
+    /// <summary>
+    ///  Method to be used when a plant is chosen in the shop menu.\
+    /// </summary>
+    /// <param name="crop_SO"></param>
+    public void Plant(Crop_SO crop_SO)
+    {
+        // Asserts
+        Debug.Assert(crop_SO != null, "crop SO cannot be null!");
+        Debug.Assert(CurrentPlotState == PlotState.EMPTY, "Cannot plant while plot is not empty!");
+        Debug.Assert(crop_SO.baseTimeNeededPerClaim >= TimeSpan.Zero, "Time needed cannot be negative!");
+
+        this.crop_SO = crop_SO;
+
+        // Success
+        GrowthFinishTime = DateTime.Now.Add(crop_SO.baseTimeNeededPerClaim);
+        CurrentPlotState = PlotState.GROWING;
+
+        //Visual update
+        //TODO: Possible optimization, use crop pools.
+        InstantiateCrop(crop_SO);
     }
 
     /// <summary>
