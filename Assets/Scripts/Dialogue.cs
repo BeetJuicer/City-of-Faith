0b034bbb8e4e3f173fff3ac990e20c750a8a7e08
AldrinEd <edullantesjohnaldrin54@gmail.com>
2024-12-03 15:22:57 -1200

    Dialogue UI and Prefabs of UI's

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public TMP_Text textComponent;
+    public Button nextButton;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        nextButton.onClick.AddListener(OnNextButtonClick);
+        StartDialogue();
+    }
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+        nextButton.interactable = true;
+    }
+
+    void OnNextButtonClick()
+    {
+        nextButton.interactable = false;
+
+        if (textComponent.text == lines[index])
+        {
+            NextLine();
+        }
+        else
+        {
+            StopAllCoroutines();
+            textComponent.text = lines[index];
+            nextButton.interactable = true;
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
