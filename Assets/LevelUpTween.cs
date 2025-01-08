9b1de60916c835001cdb192d067d83e12360f6b0
Xiaoranxi <164878289+Xiaoranxi@users.noreply.github.com>
2025-01-08 14:45:03 +0800

    LevelUpScene pt.1

---
+++ Assets/LevelUpTween.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUpTween : MonoBehaviour
+{
+    [SerializeField] GameObject SoftWheel, HardWheel, HUD;
+
+    public void OnEnable()
+    {
+        HUD.SetActive(false);
+        LeanTween.rotateAround(SoftWheel, Vector3.forward, -360, 10f).setLoopClamp();
+        LeanTween.rotateAround(HardWheel, Vector3.forward, 360, 10f).setLoopClamp();
+    }
+
+    public void CloseLevel()
+    {
+        OnComplete();
+    }
+
+    public void OnComplete()
+    {
+        SoftWheel.SetActive(false);
+        HardWheel.SetActive(false);
+        gameObject.SetActive(false);
+        HUD.SetActive(true);
+    }
+}
---
+++ Assets/OpenCropTween.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCropTween : MonoBehaviour
+{
+    [SerializeField] GameObject CropShop;
+
+    public void OnEnable()
+    {
+        CropShop.transform.localScale = Vector3.zero;
+        LeanTween.scale(CropShop, Vector3.one, 0.8f).setEase(LeanTweenType.easeOutExpo).setDelay(0.2f);
+    }
+
+    public void CloseBox()
+    {
+        CropShop.LeanMoveLocalX(Screen.width, 0.5f).setEaseInExpo().setOnComplete(OnComplete);
+    }
+
+    void OnComplete()
+    {
+        gameObject.SetActive(false);
+        CropShop.SetActive(false );
+    }
+}
