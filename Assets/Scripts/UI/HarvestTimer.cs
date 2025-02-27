0bc05236f7c6af39303016f5793e09dd8d0018b6
AldrinEd <165240912+AldrinEd@users.noreply.github.com>
2025-02-27 19:29:19 +0800

    Harvest TImer

--- Assets/Scripts/UI/HarvestTimer.cs
+++ Assets/Scripts/UI/HarvestTimer.cs
@@ -9,64 +9,43 @@ using static Database;
 
 public class HarvestTimer : MonoBehaviour
 {
-    private Crop_SO crop_so;
     private Plot plot;
-    [SerializeField] private Slider progressSlider;
+    [SerializeField] private Slider timerSlider;
     [SerializeField] private TMP_Text timerText;
     [SerializeField] private GameObject timerUI;
 
 
-    private DateTime timeBuildStarted;
-    private DateTime timeBuildFinished;
-
     private void Start()
     {
-        plot = GetComponentInParent<Plot>();
-        crop_so = plot.Crop_SO2;
-        if (crop_so != null)
-        {
-            InitializeBuildTimer();
-        }
-        else
-        {
-            Debug.LogError("Crop_SO2 is null. Timer will not work!");
-        }
-    }
-
-    private void InitializeBuildTimer()
-    {
-        timeBuildStarted = DateTime.Now;
-        timeBuildFinished = timeBuildStarted
-                            .AddDays(crop_so.daysToClaim)
-                            .AddHours(crop_so.hoursToClaim)
-                            .AddMinutes(crop_so.minutesToClaim)
-                            .AddSeconds(crop_so.secondsToClaim);
-        timerUI.SetActive(true);
+        plot = GetComponentInParent<Plot>(); // Assumes UI is a child of the plot
+        timerSlider.gameObject.SetActive(false); // Hide initially
     }
 
     private void Update()
     {
-        UpdateTimerUI();
-    }
+        if (plot == null || timerSlider == null)
+            return;
 
-    private void UpdateTimerUI()
-    {
-        TimeSpan remainingTime = timeBuildFinished - DateTime.Now;
-
-        if (remainingTime.TotalSeconds > 0)
+        if (plot.CurrentPlotState == Plot.PlotState.GROWING)
         {
-            timerText.text = string.Format("{0:D2}:{1:D2}:{2:D2}", remainingTime.Hours, remainingTime.Minutes, remainingTime.Seconds);
-            float totalBuildTime = (float)(timeBuildFinished - timeBuildStarted).TotalSeconds;
-            float remainingBuildTime = (float)remainingTime.TotalSeconds;
-            progressSlider.value = Mathf.Clamp01(1 - (remainingBuildTime / totalBuildTime));
+            timerSlider.gameObject.SetActive(true); // Show the slider when growing
+            timerText.gameObject.SetActive(true);
+            UpdateSlider();
         }
         else
         {
-            progressSlider.value = 1f;
-
-            // Hide the timer UI once the build is complete
-            timerUI.SetActive(false);
-            enabled = false; // Stop updating the script
+            timerSlider.gameObject.SetActive(false); // Hide when not growing
+            timerText.gameObject.SetActive(false);
         }
+    }
+
+    private void UpdateSlider()
+    {
+        TimeSpan totalDuration = plot.GetTotalDuration();
+        TimeSpan remainingTime = plot.GrowthFinishTime - DateTime.Now;
+
+        float progress = 1f - (float)(remainingTime.TotalSeconds / totalDuration.TotalSeconds);
+        timerSlider.value = Mathf.Clamp01(progress); // Ensures value stays between 0 and 1
+        timerText.text = string.Format("{0:D2}:{1:D2}:{2:D2}", remainingTime.Hours, remainingTime.Minutes, remainingTime.Seconds);
     }
 }
