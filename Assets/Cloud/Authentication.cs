cd02e365c003abfde0d17da41aa58e3a7f7bc156
Carl <94771147+BeetJuicer@users.noreply.github.com>
2025-03-05 16:36:09 +0800

    facebook sdk

--- Assets/Cloud/Authentication.cs
+++ Assets/Cloud/Authentication.cs
@@ -49,4 +49,72 @@ public class Authentication : MonoBehaviour
             Debug.LogException(ex);
         }
     }
+
+    async Task LinkWithFacebookAsync(string token)
+    {
+        try
+        {
+            await AuthenticationService.Instance.LinkWithFacebookAsync(token);
+            Debug.Log("Link is successful.");
+        }
+        catch (AuthenticationException ex) when (ex.ErrorCode == AuthenticationErrorCodes.AccountAlreadyLinked)
+        {
+            // Prompt the player with an error message.
+            Debug.LogError("This user is already linked with another account. Log in instead.");
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+
+    async Task SignInWithFacebookAsync(string token)
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInWithFacebookAsync(token);
+            Debug.Log("SignIn is successful.");
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+
+    async Task UnlinkFacebookAsync()
+    {
+        try
+        {
+            await AuthenticationService.Instance.UnlinkFacebookAsync();
+            Debug.Log("Unlink is successful.");
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
 }
