e8bfd554e590088ccbe8432e203fc3f8a9d0fbe7
Carl Cap <94771147+BeetJuicer@users.noreply.github.com>
2025-04-18 21:55:26 +0800

    cloud code pt1

--- Assets/FriendHallCanvas.cs
+++ Assets/FriendHallCanvas.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,16 +8,62 @@ public class FriendHallCanvas : MonoBehaviour, ICanvasView
     [SerializeField] private GameObject panel;
     [SerializeField] private Button donateBtn;
     [SerializeField] private Button exitBtn;
+    [SerializeField] private Button plusBtn;
+    [SerializeField] private Button minusBtn;
+    [SerializeField] private TextMeshProUGUI goldDonationAmount;
 
     public event Action Canvas_Deactivated;
-    public event Action Donate_Clicked;
+    public event Action<int> Donate_Clicked;
+
+    private int[] goldDonationTiers = new int[] { 1, 5, 10, 15 };
+    private int currentGoldDonationTier;
 
     private void Start()
     {
         exitBtn.onClick.AddListener(Deactivate);
-        donateBtn.onClick.AddListener(() => Donate_Clicked?.Invoke());
+        donateBtn.onClick.AddListener(() => Donate_Clicked?.Invoke(goldDonationTiers[currentGoldDonationTier]));
+        plusBtn.onClick.AddListener(Add);
+        minusBtn.onClick.AddListener(Subtract);
+
+        currentGoldDonationTier = 0;
+        goldDonationAmount.text = goldDonationTiers[currentGoldDonationTier].ToString();
+        minusBtn.interactable = false;
     }
 
+    private void Add()
+    {
+        if (currentGoldDonationTier + 1 >= goldDonationTiers.Length)
+        {
+            plusBtn.interactable = false;
+            return;
+        }
+
+        currentGoldDonationTier++;
+        goldDonationAmount.text = goldDonationTiers[currentGoldDonationTier].ToString();
+        minusBtn.interactable = true;
+
+        if (currentGoldDonationTier == goldDonationTiers.Length - 1)
+        {
+            plusBtn.interactable = false;
+        }
+    }
+    private void Subtract()
+    {
+        if (currentGoldDonationTier == 0)
+        {
+            minusBtn.interactable = false;
+            return;
+        }
+
+        currentGoldDonationTier--;
+        goldDonationAmount.text = goldDonationTiers[currentGoldDonationTier].ToString();
+        plusBtn.interactable = true;
+
+        if (currentGoldDonationTier == 0)
+        {
+            minusBtn.interactable = false;
+        }
+    }
 
     private void OnDestroy()
     {
